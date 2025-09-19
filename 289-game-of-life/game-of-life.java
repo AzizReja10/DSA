dc6201b23ba4660871f98fe357e52a2bbf5e6603
Aziz Reja <rejaaziz686@gmail.com>
2025-09-19 10:44:29 +0530

    Time: 11 ms (3.48%) | Memory: 42 MB (25.66%) - LeetSync

---
+++ 289-game-of-life/game-of-life.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+    
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sent=game(board,i,j,m,n);
+                if(board[i][j]==1&&(sent<2||sent>3))
+                    board[i][j]=-1;
+                if(board[i][j]==0&&sent==3)
+                    board[i][j]=2;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(board[i][j]==-1)
+                board[i][j]=0;
+                else if(board[i][j]==2)
+                board[i][j]=1;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static int game(int[][] boards,int i,int j,int m,int n)
+    {
+        int count=0;
+        for(int a=-1;a<2;a++)
+        {
+            for(int b=-1;b<2;b++)
+            {
+                int r=a+i,c=b+j;
+                if(r==i&&c==j)
+                    continue;
+                if(r>=0&&r<m&&c>=0&&c<n)
+                {
+                    if (Math.abs(boards[r][c]) == 1) { 
+                        count++;
+                    }
+                }
+            }
+        }
+        return(count);
+    }
+}
