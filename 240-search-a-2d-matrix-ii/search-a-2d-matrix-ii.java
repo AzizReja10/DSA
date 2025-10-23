41fcaf524df6f9f77e898dfa4cc0838c30553494
Aziz Reja <rejaaziz686@gmail.com>
2025-10-23 11:26:01 +0530

    Time: 6 ms (47.09%) | Memory: 46.1 MB (41.02%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Start from top-right corner
-        return search(matrix, target, 0, matrix.length - 1, 0, matrix[0].length - 1);
+      return divide_conquer(matrix, target, 0, matrix.length - 1, 0, matrix[0].length - 1);
+        //return search(matrix, target, 0, matrix.length - 1, 0, matrix[0].length - 1);
     }
 
     static boolean search(int[][] matrix, int target, int top, int bottom, int left, int right) {
@@ -11,10 +11,24 @@ class Solution {
             if (matrix[row][right] == target)
                 return true;
             else if (matrix[row][right] > target)
-                right--; // move left
+                right--; 
             else
-                row++;   // move down
+                row++;  
         }
         return false;
+    }
+    static  boolean divide_conquer(int[][] matrix, int target, int top, int bottom, int left, int right)
+    {
+        if(top>bottom|| left>right)
+        return false;
+        int row=top;
+        int mid=left+(right-left)/2;
+        while(row<=bottom&&matrix[row][mid]<=target)
+        {
+            if(matrix[row][mid]==target)
+            return true;
+            row++;
+        }
+        return(divide_conquer(matrix,target,row,bottom,left,mid-1)||divide_conquer(matrix,target,top,row-1,mid+1,right));
     }
 }
