795c2b4effac8d88ccc53b5594a78f7708f51b94
Aziz Reja <rejaaziz686@gmail.com>
2025-11-01 00:05:24 +0530

    Time: 1 ms (99.84%) | Memory: 46.3 MB (6.36%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -4,26 +4,23 @@ class Solution {
             boolean[] row = new boolean[9];
             boolean[] col = new boolean[9];
             boolean[] box = new boolean[9];
-
             for (int j = 0; j < 9; j++) {
-                // Row
+                // row
                 if (board[i][j] != '.') {
                     int num = board[i][j] - '1';
                     if (row[num]) return false;
                     row[num] = true;
                 }
-
-                // Column
+                // column
                 if (board[j][i] != '.') {
                     int num = board[j][i] - '1';
                     if (col[num]) return false;
                     col[num] = true;
                 }
-
-                // 3×3 box
-                int rowIndex = 3 * (i / 3);
-                int colIndex = 3 * (i % 3);
-                char c = board[rowIndex + j / 3][colIndex + j % 3];
+                //box
+                int crow = 3 * (i / 3);
+                int ccol = 3 * (i % 3);
+                char c = board[crow + j / 3][ccol + j % 3];
                 if (c != '.') {
                     int num = c - '1';
                     if (box[num]) return false;
