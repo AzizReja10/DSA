ddc13ad47a31e8202a0b6b2971d7b88a1aeb6ff7
Aziz Reja <rejaaziz686@gmail.com>
2025-11-06 11:42:42 +0530

    Time: 23 ms (25.59%) | Memory: 47.8 MB (5.68%) - LeetSync

--- 475-heaters/heaters.java
+++ 475-heaters/heaters.java
@@ -1,12 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
     public int findRadius(int[] houses, int[] heaters) {
-        int radius=0;
-        for(int i:houses)
-        {
-        int min=Integer.MAX_VALUE;
-        for(int j:heaters)
-        min=Math.min(min,Math.abs(i-j));
-        radius=Math.max(min,radius);
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int radius = 0;
+        for (int house : houses) {
+            int left = 0, right = heaters.length - 1;
+            int min = Integer.MAX_VALUE;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                int diff = heaters[mid] - house;
+                min = Math.min(min, Math.abs(diff));
+                if (heaters[mid] < house)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            radius = Math.max(radius, min);
         }
         return radius;
     }
