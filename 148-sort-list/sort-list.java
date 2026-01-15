2c7a9986398fc6723c667969ae2b5f9bd68b001e
Aziz Reja <rejaaziz686@gmail.com>
2026-01-15 11:41:34 +0530

    Time: 9 ms (98.55%) | Memory: 59.2 MB (75.82%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
+    public ListNode merge(ListNode head1,ListNode head2)
+    {
+      ListNode dummy=new ListNode(-1);
+      ListNode curr=dummy;
+      while(head1!=null&&head2!=null)
+      {
+        if(head1.val<=head2.val)
+        {
+            curr.next=head1;
+            head1=head1.next;
+        }
+        else
+        {
+            curr.next=head2;
+            head2=head2.next;
+        }
+        curr=curr.next;
+      }
+      if(head1!=null)
+      curr.next=head1;
+      if(head2!=null)
+      curr.next=head2;
+      return dummy.next;
+    }
+    public ListNode mergeSort(ListNode head)
+    {
+        if(head==null||head.next==null)
+        return head;
+        ListNode mid=getMid(head);
+        ListNode rightH=mid.next;
+        mid.next=null;
+        ListNode leftS=mergeSort(head);
+        ListNode rightS=mergeSort(rightH);
+        return merge(leftS,rightS);
+    }
+    public ListNode getMid(ListNode head)
+    {
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null&&fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
