b80e943cd9b1406dcb495fcee3e77e062a127ee7
Aziz Reja <rejaaziz686@gmail.com>
2026-03-08 00:21:29 +0530

    Time: 9 ms (60.36%) | Memory: 48.8 MB (88.99%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.java
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -3,23 +3,14 @@ import java.util.Arrays;
 class Solution {
     public void wiggleSort(int[] nums) {
         int n = nums.length;
-        // 1. Sort the array to easily pick small and large elements
         int[] sorted = nums.clone();
         Arrays.sort(sorted);
-        
-        // 2. Use two pointers starting from the end of each half
-        // Left half: 0 to (n-1)/2
-        // Right half: (n-1)/2 + 1 to n-1
         int left = (n - 1) / 2; 
         int right = n - 1;
-        
-        // 3. Fill the original array
         for (int i = 0; i < n; i++) {
-            // Even indices get the smaller values
             if (i % 2 == 0) {
                 nums[i] = sorted[left--];
             } 
-            // Odd indices get the larger values
             else {
                 nums[i] = sorted[right--];
             }
