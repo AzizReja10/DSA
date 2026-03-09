3e681da6c10f51f7d52256d1f7ef1eaf5e6f3fb6
Aziz Reja <rejaaziz686@gmail.com>
2026-03-09 23:40:56 +0530

    Time: 8 ms (74.83%) | Memory: 65.3 MB (55.34%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -9,23 +9,15 @@ public class Solution {
     }
 
     private void backtrack(String s, int start, List<String> currentList, List<List<String>> result) {
-        // Base case: If we've reached the end of the string, add the current partition to results
         if (start == s.length()) {
             result.add(new ArrayList<>(currentList));
             return;
         }
 
         for (int end = start; end < s.length(); end++) {
-            // Check if the substring from 'start' to 'end' is a palindrome
             if (isPalindrome(s, start, end)) {
-                // Add the valid substring to our path
                 currentList.add(s.substring(start, end + 1));
-                
-                // Move the pointer forward to the next character after 'end'
-                backtrack(s, end + 1, currentList, result);
-                
-                // Remove the last element (backtrack) to try the next possible split
-                currentList.remove(currentList.size() - 1);
+                backtrack(s, end + 1, currentList, result);                currentList.remove(currentList.size() - 1);
             }
         }
     }
