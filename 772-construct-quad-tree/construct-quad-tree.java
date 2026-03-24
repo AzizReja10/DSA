9bbed0d005df6967573440eeac2e933e92dd6369
Aziz Reja <rejaaziz686@gmail.com>
2026-03-24 22:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (78.89%) - LeetSync

---
+++ 772-construct-quad-tree/construct-quad-tree.java
@@ -0,0 +1,72 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return build(grid, 0, 0, grid.length);
+    }
+
+    private Node build(int[][] grid, int r, int c, int size) {
+        if (isAllSame(grid, r, c, size)) {
+            // If all values are the same, this is a leaf node
+            return new Node(grid[r][c] == 1, true);
+        }
+
+        // If not same, split into 4 quadrants
+        int half = size / 2;
+        Node topLeft = build(grid, r, c, half);
+        Node topRight = build(grid, r, c + half, half);
+        Node bottomLeft = build(grid, r + half, c, half);
+        Node bottomRight = build(grid, r + half, c + half, half);
+
+        // Current node is not a leaf, value can be anything (usually true/1)
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
+    private boolean isAllSame(int[][] grid, int r, int c, int size) {
+        int val = grid[r][c];
+        for (int i = r; i < r + size; i++) {
+            for (int j = c; j < c + size; j++) {
+                if (grid[i][j] != val) return false;
+            }
+        }
+        return true;
+    }
+}
