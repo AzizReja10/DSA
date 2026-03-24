807118de2021eb26148c0f702091c9d374db322c
Aziz Reja <rejaaziz686@gmail.com>
2026-03-24 22:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (34.70%) - LeetSync

--- 772-construct-quad-tree/construct-quad-tree.java
+++ 772-construct-quad-tree/construct-quad-tree.java
@@ -42,24 +42,17 @@ class Solution {
     public Node construct(int[][] grid) {
         return build(grid, 0, 0, grid.length);
     }
-
     private Node build(int[][] grid, int r, int c, int size) {
         if (isAllSame(grid, r, c, size)) {
-            // If all values are the same, this is a leaf node
             return new Node(grid[r][c] == 1, true);
         }
-
-        // If not same, split into 4 quadrants
         int half = size / 2;
         Node topLeft = build(grid, r, c, half);
         Node topRight = build(grid, r, c + half, half);
         Node bottomLeft = build(grid, r + half, c, half);
         Node bottomRight = build(grid, r + half, c + half, half);
-
-        // Current node is not a leaf, value can be anything (usually true/1)
         return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
     }
-
     private boolean isAllSame(int[][] grid, int r, int c, int size) {
         int val = grid[r][c];
         for (int i = r; i < r + size; i++) {
