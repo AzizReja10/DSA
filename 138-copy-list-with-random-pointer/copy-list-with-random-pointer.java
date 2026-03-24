e0d028d2e3a625cb90e96b788839aa317e21e3f8
Aziz Reja <rejaaziz686@gmail.com>
2026-03-24 22:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (44.08%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -16,8 +16,6 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         if (head == null) return null;
-
-        // Step 1: Create interleaved nodes
         Node curr = head;
         while (curr != null) {
             Node copy = new Node(curr.val);
@@ -25,32 +23,22 @@ class Solution {
             curr.next = copy;
             curr = copy.next;
         }
-
-        // Step 2: Assign random pointers for the copies
         curr = head;
         while (curr != null) {
             if (curr.random != null) {
-                // The copy's random is the copy of the original's random
                 curr.next.random = curr.random.next;
             }
             curr = curr.next.next;
         }
-
-        // Step 3: Separate the two lists
         curr = head;
         Node dummy = new Node(0);
         Node copyCurr = dummy;
-        
         while (curr != null) {
-            // Extract the copy
             copyCurr.next = curr.next;
             copyCurr = copyCurr.next;
-            
-            // Restore the original list
             curr.next = curr.next.next;
             curr = curr.next;
         }
-
         return dummy.next;
     }
 }
